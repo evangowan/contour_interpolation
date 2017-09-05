3738a93a21c665ff87c4383c02d3ceeff7df9b6f
evangowan <evangowan@gmail.com>
2017-09-05 14:57:07 +0200

    fixed issue related to oscillating between grid cells, and also problem with flowline increment

--- flowlines.f90
+++ flowlines.f90
@@ -6,32 +6,36 @@ program flowlines2
 	use read_minmax
 	use boundary_mask_mod
 	use direction_grid_mod
+	use crossover
 
 	implicit none
 
 	integer :: commandline_count, polygon_counter, points_counter, x_grid_index, y_grid_index, x_dummy_index, y_dummy_index
 	integer :: check_peak, x_increment, y_increment, index_next, extra_counter, extra_total, flowline_point_count, flow_counter
 	integer :: max_line, max_point
-	integer, parameter :: gmt_unit = 90, max_flowline_points = 100000
+	integer, parameter :: gmt_unit = 90, max_flowline_points = 100000, discard_unit=200
 
 	double precision :: grid_spacing, current_x, current_y, current_direction, next_x, next_y, grid_x, grid_y, temp_x, temp_y
 	
-	double precision :: crossover_x, crossover_y, angle, closeness_threshold
-	double precision, parameter :: r_increment = 0.25, threshold_factor = 100.
+	double precision :: crossover_x, crossover_y, angle, closeness_threshold, r_increment
+	double precision, parameter :: r_increment_minimum = 0.02, threshold_factor = 100.
 
 	double precision, dimension(max_flowline_points) :: x_flowline_store, y_flowline_store
 
 
 	double precision, parameter :: pi = 3.141592653589793, to_round_down = 0.00001
 
+	integer :: last_x_index, last_y_index
+	integer, allocatable, dimension(:,:) :: visited_grid
+
 	character(len=256) :: arg_in
 	character (len=1), parameter :: divider_character = ">"
-	character(len=256), parameter :: gmt_file = "flowlines.txt"
+	character(len=256), parameter :: gmt_file = "flowlines.txt", discard_file="discarded_flowlines.txt"
 
 
-	logical :: found_point, end_line, round_down_x, round_down_y, looping, hit_saddle
+	logical :: found_point, end_line, round_down_x, round_down_y, looping, hit_saddle, oscillating
 
-	closeness_threshold = r_increment / threshold_factor
+
 
 
 	commandline_count = command_argument_count()
@@ -44,6 +48,10 @@ program flowlines2
 
 	call GET_COMMAND_ARGUMENT(1,arg_in) 
 	read(arg_in,*) grid_spacing
+
+	r_increment = grid_spacing * r_increment_minimum
+
+	closeness_threshold = r_increment / threshold_factor
 
 	! read polygons
 	call read_polygons_init()
@@ -58,17 +66,22 @@ program flowlines2
 	call read_direction_grid_init(number_x_grid,number_y_grid)
 	call read_direction_grid(number_x_grid,number_y_grid)
 
+	allocate(visited_grid(number_x_grid,number_y_grid))
 
 	! write GMT file for plotting/debugging
 
 	open(unit=gmt_unit, file=gmt_file, access="sequential", form="formatted", status="replace")
 
-	max_line = 0
+	max_line = 0 
 	max_point = 0
+
+	open(unit=discard_unit, file=discard_file, access="sequential", form="formatted", status="replace")
+
 	! at each initial boundary point, go out until it reaches the secondary boundary
 	do polygon_counter = 1, number_polygons(1)
 		do points_counter = 1, polygon_points(1, polygon_counter)
-	
+			write(6,*) "polygon: ", polygon_counter, "/", number_polygons(1), " point:", points_counter, "/", &
+				polygon_points(1, polygon_counter)
 			if(points_counter < polygon_points(1, polygon_counter) ) then
 				index_next = points_counter + 1
 			else
@@ -86,8 +99,11 @@ program flowlines2
 			call find_grid_index(x_flowline_store(flowline_point_count), y_flowline_store(flowline_point_count),&
 			   grid_spacing, x_grid_index, y_grid_index)
 
+			visited_grid = 0
+			last_x_index = x_grid_index
+			last_y_index = y_grid_index
 
-
+			oscillating = .false.
 			flowline_loop: do
 				! find grid points
 				flowline_point_count = flowline_point_count + 1
@@ -96,6 +112,7 @@ program flowlines2
 
 				call find_grid_location(x_grid_index, y_grid_index, grid_spacing, grid_x, grid_y)
 
+
 				current_direction = direction_grid(x_grid_index, y_grid_index)
 
 				x_flowline_store(flowline_point_count) = x_flowline_store(flowline_point_count-1) +&
@@ -103,6 +120,20 @@ program flowlines2
 				y_flowline_store(flowline_point_count) = y_flowline_store(flowline_point_count-1) +&
 				  r_increment * sin(current_direction)
 
+				if(x_grid_index /= last_x_index .or. y_grid_index /= last_y_index) THEN
+					visited_grid(last_x_index,last_y_index) = visited_grid(last_x_index,last_y_index) + 1
+				endif
+				
+				last_x_index = x_grid_index
+				last_y_index = y_grid_index
+
+
+				if(visited_grid(x_grid_index,y_grid_index) > 2) THEN
+					write(6,*) "likely oscillating"
+					oscillating = .true.
+					exit flowline_loop
+				endif
+				
 
 				if(isnan(x_flowline_store(flowline_point_count))) THEN
 					write(6,*) current_direction
@@ -190,12 +221,19 @@ program flowlines2
 					max_point = flowline_point_count
 					max_line = points_counter
 				endif
-
-				write(gmt_unit,'(A1,I7,I7)') divider_character, points_counter, flowline_point_count
+				if (oscillating) THEN
+				!	write(gmt_unit,'(A1,I7,I7)') divider_character, points_counter, 0
+					write(discard_unit,'(A1,I7,I7)') divider_character, points_counter, flowline_point_count
+				else
+					write(gmt_unit,'(A1,I7,I7)') divider_character, points_counter, flowline_point_count
+				endif
 
 				do flow_counter = 1, flowline_point_count, 1
-
-					write(gmt_unit,*) x_flowline_store(flow_counter), y_flowline_store(flow_counter)
+					if(oscillating) THEN
+						write(discard_unit,*) x_flowline_store(flow_counter), y_flowline_store(flow_counter)
+					else
+						write(gmt_unit,*) x_flowline_store(flow_counter), y_flowline_store(flow_counter)
+					endif
 
 				end do
 			endif
@@ -210,213 +248,12 @@ program flowlines2
 
 	close(unit=gmt_unit)
 
+	deallocate(visited_grid)
+
 	! clear memory
 	call read_polygons_clear()
 	call boundary_mask_clear()
 	call read_direction_grid_clear()
 
-contains
-
-subroutine cross_polygon(current_x, current_y, next_x, next_y, end_line, grid_spacing)
-
-	! find out if there is an overlap between the current line segment and the inner polygons
-
-	use read_polygons
-
-	implicit none
-
-	double precision, intent(in) :: current_x, current_y, grid_spacing
-	double precision, intent(inout) :: next_x, next_y
-	logical, intent(inout) :: end_line
-
-
-
-	integer :: polygon_counter, points_counter, next_index
-
-
-	double precision :: seg_x1, seg_x2, seg_y1, seg_y2, temp_x, temp_y
-
-	logical :: is_crossover
-
-	end_line = .false.
-
-
-	polygon_loop: do polygon_counter = 1, number_polygons(2), 1
-		points_loop: do points_counter = 1, polygon_points(2, polygon_counter), 1
-
-			next_index = points_counter + 1
-			if(next_index > polygon_points(2, polygon_counter)) then
-				next_index = 1
-			endif
-
-			seg_x1 = x_coordinates(2,polygon_counter,points_counter)
-			seg_y1 = y_coordinates(2,polygon_counter,points_counter)
-
-			seg_x2 = x_coordinates(2,polygon_counter,next_index)
-			seg_y2 = y_coordinates(2,polygon_counter,next_index)
-
-
-			call crossover_point(current_x, current_y, next_x, next_y, seg_x1, seg_y1, seg_x2, seg_y2, &
-			  temp_x, temp_y, is_crossover)
-
-			if( is_crossover ) THEN ! crossover
-				next_x = temp_x
-				next_y = temp_y
-				end_line = .true.
-				write(546,*) next_x, next_y
-				exit polygon_loop
-			endif
-
-
-		end do points_loop
-	end do polygon_loop
-
-
-end subroutine cross_polygon
-
-
-
-subroutine cross_itself(x_flowline_store, y_flowline_store, flowline_point_count, end_line)
-	! checks to see if the line crosses over itself, will adjust flowline_point_count if it does
-
-	implicit none
-	integer, intent(inout) :: flowline_point_count
-	double precision, dimension(flowline_point_count), intent(inout) :: x_flowline_store, y_flowline_store
-	logical, intent(out) :: end_line
-
-	integer :: point_counter, end_index
-
-	double precision :: a_x1, a_y1, a_x2, a_y2, b_x1, b_y1, b_x2, b_y2, crossover_x, crossover_y
-	logical :: is_crossover
-
-	! for ease of reading
-
-	a_x1 = x_flowline_store(flowline_point_count-1)
-	a_y1 = y_flowline_store(flowline_point_count-1)
-	a_x2 = x_flowline_store(flowline_point_count)
-	a_y2 = y_flowline_store(flowline_point_count)
-	is_crossover = .false.
-
-	point_loop: do point_counter = 1, flowline_point_count-3, 1 ! shouldn't need to check the last line, unless there is a freak case where the line goes completely the opposite way
-
-		b_x1 = x_flowline_store(point_counter)
-		b_y1 = y_flowline_store(point_counter)
-		b_x2 = x_flowline_store(point_counter+1)
-		b_y2 = y_flowline_store(point_counter+1)
-		
-		call crossover_point(a_x1, a_y1, a_x2, a_y2, b_x1, b_y1, b_x2, b_y2, crossover_x, crossover_y, is_crossover)
-
-		if(is_crossover) THEN
-			write(548,*) ">", point_counter, flowline_point_count
-			write(548,*) a_x1, a_y1,b_x1, b_y1,  crossover_x, crossover_y
-			write(548,*) a_x2, a_y2,b_x2, b_y2
-			end_index = point_counter
-			exit point_loop
-
-		end if
-
-	end do point_loop
-
-	if(is_crossover) THEN
-
-	!	write(6,*) end_index, flowline_point_count
-		flowline_point_count = end_index
-		x_flowline_store(end_index) = crossover_x
-		y_flowline_store(end_index) = crossover_y
-		end_line = .true.
-		write(547,*) crossover_x, crossover_y
-	else
-		end_line = .false.
-	end if
-
-end subroutine cross_itself
-
-
-subroutine crossover_point(a_x1, a_y1, a_x2, a_y2, b_x1, b_y1, b_x2, b_y2, crossover_x, crossover_y, is_crossover)
-
-	! checks if the two given line segments overlap. If they do, this subroutine returns the crossover point
-
-	implicit none
-
-	double precision, intent(in) :: a_x1, a_y1, a_x2, a_y2, b_x1, b_y1, b_x2, b_y2
-	double precision, intent(out) :: crossover_x, crossover_y
-	logical, intent(out) :: is_crossover
-
-	double precision :: slope1, slope2, intercept1, intercept2, a_min_cell_x, a_min_cell_y, a_max_cell_x, a_max_cell_y
-	double precision :: b_min_cell_x, b_min_cell_y, b_max_cell_x, b_max_cell_y
-	double precision :: temp_x, temp_y1, temp_y2
-	double precision, parameter :: threshold = 1e6
-
-	logical :: vertical_line1, vertical_line2
-
-	if(a_x1 /= a_x2) THEN
-		
-		
-		vertical_line1 = .false.
-	else
-		vertical_line1 = .true.
-	endif
-
-
-	a_min_cell_x = min(a_x1, a_x2)
-	a_min_cell_y = min(a_y1, a_y2)
-	a_max_cell_x = max(a_x1, a_x2)
-	a_max_cell_y = max(a_y1, a_y2)
-
-	b_min_cell_x = min(b_x1, b_x2)
-	b_min_cell_y = min(b_y1, b_y2)
-	b_max_cell_x = max(b_x1, b_x2)
-	b_max_cell_y = max(b_y1, b_y2)
-
-
-	is_crossover = .false.
-
-
-
-	slope1 = (a_y2 - a_y1) / (a_x2 - a_x1) 
-	intercept1 = a_y2 - a_x2 * slope1
-
-	slope2 = (b_y2 - b_y1) / (b_x2 - b_x1) 
-	intercept2 = b_y2 - b_x2 * slope2
-
-	temp_x = (intercept2 - intercept1) / (slope1 - slope2)
-	temp_y = slope1 * temp_x + intercept1
-
-	if(a_x2 == a_x1) then
-		temp_x = a_x2
-		temp_y = slope2 * temp_x + intercept2
-	elseif(b_x2 == b_x1) THEN
-		temp_x = b_x2
-		temp_y = slope1 * temp_x + intercept1
-	endif
-
-
-	is_crossover = .false.
-	if(temp_x > a_min_cell_x) THEN
-	  if(temp_x < a_max_cell_x) THEN
-	     if(temp_x > b_min_cell_x) THEN
-		 if(temp_x < b_max_cell_x) THEN
-	   	   if(temp_y > a_min_cell_y) THEN
-		     if(temp_y < a_max_cell_y) THEN
-	   		 if(temp_y > b_min_cell_y) THEN
-			   if(temp_y < b_max_cell_y) THEN
-
-				is_crossover = .true.
-			   endif
-			 endif
-		     endif
-		   endif
-		 endif
-	     endif
-	   endif
-	endif
-
-
-
-
-
-	crossover_x = temp_x
-	crossover_y = temp_y
-end subroutine crossover_point
 
 end program flowlines2
